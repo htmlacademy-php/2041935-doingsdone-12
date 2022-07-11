8368de51a6b9406b56718d2e865df95e20aaa9d5
Фахрадов Эмиль <FakhradovE@GCSU-555.LOC>
2022-07-11 12:34:30 +0300

    Правки

--- index.php
+++ index.php
@@ -72,7 +72,8 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1): echo'checked'; endif; ?>>
+
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?= ($show_complete_tasks == 1) ? 'checked' : '' ; ?> >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -92,6 +93,7 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
+
 
 
                           <?php if($show_complete_tasks == 1): ?>
